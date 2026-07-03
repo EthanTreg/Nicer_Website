89d9dde95814208f83f263c51f9885f57619b02c
rrocketter <rahul.rocket711@gmail.com>
2026-07-02 22:17:21 -0400

    fix settings.py db setup

--- nicer_website/settings.py
+++ nicer_website/settings.py
@@ -90,12 +90,13 @@ WSGI_APPLICATION = 'nicer_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# Using SQLite for development (easier setup than PostgreSQL)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': CONFIG['database_name'],
+        'HOST': 'localhost',
+        'PORT': '5432',
+    } | DB_USER
 }
 
 
